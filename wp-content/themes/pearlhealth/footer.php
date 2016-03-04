bb13771989258fe5dfcd4276caaed8afb17238de
iftekar007 <hiifte@gmail.com>
2016-03-04 17:10:07 +0530

    5.09 pm 4th of march

---
+++ wp-content/themes/pearlhealth/footer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: iftekar
+ * Date: 4/3/16
+ * Time: 4:22 PM
+ */
+
+?>
+
+<div id="footer">
+    <h1>FOOTER</h1>
+</div>
+</div>
+</body>
+</html>
---
+++ wp-content/themes/pearlhealth/header.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: iftekar
+ * Date: 4/3/16
+ * Time: 4:20 PM
+ */
+
+?>
+
+<html>
+<head>
+    <title>Tutorial theme</title>
+    <!--<link rel="stylesheet" href="<?php /*bloginfo('stylesheet_url'); */?>">-->
+</head>
+<body>
+<div id="wrapper">
+    <div id="header">
+        <h1>HEADER</h1>
+    </div>
